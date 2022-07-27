0ab58bcd64891b7b591a9974a5683536df378c1f
Maximiliano Muñoz Cavalcanti <95191637+maxmcavalcanti@users.noreply.github.com>
2022-07-27 15:05:00 -0300

    Backend update

--- index.js
+++ index.js
@@ -34,14 +34,6 @@ app.get('/home', (req,res) => {
 } )
 
 //Conectando na API
-mongoose
-  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cqw2a.mongodb.net/?retryWrites=true&w=majority`)
-  .then(() => {
-  console.log('Connected to MongoDB');
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
     })
-  })
-  .catch(err => {
-    console.log(err);
-  })
