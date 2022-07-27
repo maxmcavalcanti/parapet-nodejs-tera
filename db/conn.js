1f375f311014e3de2ed8439be2bd2c770c658cbc
Maximiliano Muñoz Cavalcanti <95191637+maxmcavalcanti@users.noreply.github.com>
2022-07-27 16:05:26 -0300

    backend change 3

--- db/conn.js
+++ db/conn.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+// const DB_USER = process.env.DB_USER;
+// const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 
-async function main() {
-  await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cqw2a.mongodb.net/?retryWrites=true&w=majority
-  `);
-  console.log("Connected to MongoDB");
-}
+// async function main() {
+//   await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cqw2a.mongodb.net/?retryWrites=true&w=majority
+//   `);
+//   console.log("Connected to MongoDB");
+// }
 
-main().catch((err) => console.log(err));
+// main().catch((err) => console.log(err));
 
-module.exports = mongoose;
+// module.exports = mongoose;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
-
+const mongoose = require("mongoose");
 //Config JSON response
 app.use(express.json());
 
@@ -24,8 +24,17 @@ const PetRoutes = require("./routes/PetRoutes");
 app.use("/users", UserRoutes);
 app.use("/pets", PetRoutes);
 
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
 
 //Conectando na API
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-    })
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cqw2a.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port);
+    console.log(`Conectado no banco, rodando em http://localhost:${port}`);
+  })
+  .catch((err) => console.log(err));
